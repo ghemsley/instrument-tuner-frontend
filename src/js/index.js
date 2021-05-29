daec676b721cccdb13fb95581addebf206a2d480
ghemsley <ghemsley@protonmail.ch>
2021-05-29 06:13:59 -0500

    Add displayTuning function to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -29,3 +29,31 @@ const showTuningForm = () => {
   tuningForm.appendChild(tuningFormSubmit)
   document.body.appendChild(tuningForm)
 }
+
+const displayTuning = (tuning, name = '') => {
+  const h3 = tuningNameH3() || document.createElement('h3')
+  const h4 = tuningNotesH4() || document.createElement('h4')
+  h3.id = 'tuning-name'
+  h4.id = 'tuning-notes'
+  let notes = []
+  if (typeof tuning !== 'undefined') {
+    if (typeof tuning.data !== 'undefined') {
+      notes = tuning.data.attributes.notes
+      h3.textContent = tuning.data.attributes.name
+    } else if (typeof tuning.attributes !== 'undefined') {
+      notes = tuning.attributes.notes
+      h3.textContent = tuning.attributes.name
+    } else {
+      notes = tuning.split(', ')
+      h3.textContent = name
+    }
+  }
+  h4.textContent = notes.join(' ')
+  if (!tuningNameH3()) {
+    document.body.appendChild(h3)
+  }
+  if (!tuningNotesH4()) {
+    document.body.appendChild(h4)
+  }
+  return notes
+}
