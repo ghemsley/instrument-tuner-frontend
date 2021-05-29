d855aa9a9a2139c5e873a58f236678d9505e029e
ghemsley <ghemsley@protonmail.ch>
2021-05-29 06:13:15 -0500

    Add showTuningForm function to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,19 @@ const tuningFormSubmit = () => document.getElementById('tuning-form-submit')
 
 let matchNotesInterval
 let tuner
+
+const showTuningForm = () => {
+  const tuningForm = document.createElement('form')
+  const tuningFormSelect = document.createElement('select')
+  const tuningFormSubmit = document.createElement('input')
+
+  tuningForm.id = 'tuning-form'
+  tuningFormSelect.id = 'tuning-form-select'
+  tuningFormSubmit.id = 'tuning-form-submit'
+  tuningFormSubmit.type = 'submit'
+  // tuningFormSelect.addEventListener('change', updateTuning)
+  // tuningForm.addEventListener('submit', updateTuning)
+  tuningForm.appendChild(tuningFormSelect)
+  tuningForm.appendChild(tuningFormSubmit)
+  document.body.appendChild(tuningForm)
+}
