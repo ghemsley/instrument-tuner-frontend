7534259211fe4cf5765086e58d8d91e4f6ce1eb8
ghemsley <ghemsley@protonmail.ch>
2021-05-30 23:06:52 -0500

    Update index.js, regenerate main.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import Tuning from './tuning'
+import Instrument from './instrument'
 import Tuner from './tuner'
 import '../sass/app.scss'
 
@@ -20,10 +22,10 @@ let tuner
 document.addEventListener('DOMContentLoaded', (event) => {
   createGrid()
   getInstrument(1)
+    .then((instrumentJSON) => createInstrument(instrumentJSON))
     .then((instrument) => displayInstrument(instrument))
     .then((instrument) => {
-      showTuningForm()
-      populateTuningForm(instrument)
+      showTuningForm(instrument)
     })
 })
 
@@ -54,7 +56,18 @@ const getTunings = () => fetchData('tunings')
 const getInstrument = (id) => fetchData(`instruments/${id}`)
 const getInstruments = () => fetchData('instruments')
 
-const showTuningForm = () => {
+const createInstrument = (instrumentJSON) => {
+  const name = instrumentJSON.data.attributes.name
+  const tunings = instrumentJSON.included.map(
+    (tuning) => new Tuning(tuning.attributes.name, tuning.attributes.notes)
+  )
+  return new Instrument(name, tunings)
+}
+
+const createInstruments = (instrumentsJSON) =>
+  instrumentsJSON.map((instrument) => createInstrument(instrument))
+
+const showTuningForm = (instrument) => {
   const tuningForm = document.createElement('form')
   const tuningFormSelect = document.createElement('select')
   const tuningFormSubmit = document.createElement('input')
@@ -73,75 +86,65 @@ const showTuningForm = () => {
   tuningForm.appendChild(tuningFormSubmit)
 
   container().appendChild(tuningForm)
+  return populateTuningForm(instrument)
 }
 
 const populateTuningForm = (instrument) => {
-  const tunings = instrument.included
-  for (const tuning of tunings) {
+  for (const tuning of instrument.tunings) {
     const option = document.createElement('option')
-    const string = `${tuning.attributes.name}: ${tuning.attributes.notes.join(
+    const string = `${tuning.name}: ${tuning.notes.join(
       ', '
     )}`
     option.value = string
     option.text = string
     tuningFormSelect().appendChild(option)
   }
+  return instrument
 }
 
-const displayTuning = (tuning, name = '') => {
+const displayTuning = (tuning) => {
   const h3 = tuningNameH3() || document.createElement('h3')
   const h4 = tuningNotesH4() || document.createElement('h4')
   h3.id = 'tuning-name'
   h4.id = 'tuning-notes'
-  let notes = []
-  if (typeof tuning !== 'undefined') {
-    if (typeof tuning.data !== 'undefined') {
-      notes = tuning.data.attributes.notes
-      h3.textContent = tuning.data.attributes.name
-    } else if (typeof tuning.attributes !== 'undefined') {
-      notes = tuning.attributes.notes
-      h3.textContent = tuning.attributes.name
-    } else {
-      notes = tuning.split(', ')
-      h3.textContent = name
-    }
-  }
-  h4.textContent = notes.join(' ')
+  h3.textContent = tuning.name
+  h4.textContent = tuning.notes.join(' ')
   if (!tuningNameH3()) {
     container().appendChild(h3)
   }
   if (!tuningNotesH4()) {
     container().appendChild(h4)
   }
-  return notes
+  return tuning
 }
 
 const displayTunings = (tunings) => {
-  if (typeof tunings !== 'undefined') {
-    for (const tuning of tunings) {
-      displayTuning(tuning)
-    }
-    return tunings
+  for (const tuning of tunings) {
+    displayTuning(tuning)
   }
-  return []
+  return tunings
 }
 
 const displayInstrument = (instrument) => {
-  if (typeof instrument !== 'undefined') {
-    const h1 = document.createElement('h1')
-    const tunings = instrument.included
-    h1.id = 'instrument-name'
-    h1.textContent = instrument.data.attributes.name
-    container().append(h1)
-    //displayTunings(tunings)
-  }
-  return instrument
+  return new Promise((resolve, reject) => {
+    try {
+      const h1 = document.createElement('h1')
+      h1.id = 'instrument-name'
+      h1.textContent = instrument.name
+      container().append(h1)
+      displayTunings(instrument.tunings)
+      resolve(instrument)
+    } catch (e) {
+      reject(`Error displaying instrument: ${e}`)
+    }
+  })
 }
 
 const displayInstruments = (instruments) => {
   for (const instrument of instruments) {
     displayInstrument(instrument)
   }
+  return instruments
 }
 
 const createTuner = () => {
@@ -194,15 +197,12 @@ const updateTuning = (event) => {
     typeof event.target.value === 'undefined'
       ? event.target.children[0].value.split(': ')
       : event.target.value.split(': ')
-  const tuningName = tuningPair[0]
-  const tuning = tuningPair[1]
-  displayTuning(tuning, tuningName)
-  tuningNameH3().textContent = tuningName
-  tuningNotesH4().textContent = tuning
+  const tuning = new Tuning(tuningPair[0], tuningPair[1].split(', '))
+  displayTuning(tuning)
   createTuner()
     .then((stoppedTuner) => startAndDisplayTuner(stoppedTuner, 100))
     .then((startedTuner) =>
-      highlightMatchingNotes(startedTuner, tuning.split(', '), 100)
+      highlightMatchingNotes(startedTuner, tuning.notes, 100)
     )
     .then((tuner) => tuner.drawGuage())
 }
