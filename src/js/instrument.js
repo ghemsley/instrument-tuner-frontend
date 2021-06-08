3f21d9feaaf450f251c0465c9efd46c1c9ca8bd8
ghemsley <ghemsley@protonmail.ch>
2021-06-08 07:59:29 -0500

    Refactor instrument display into instrument.js

--- src/js/instrument.js
+++ src/js/instrument.js
@@ -1,4 +1,5 @@
 import Tuning from './tuning'
+import Forms from './forms'
 
 class Instrument {
   constructor(name, tunings = []) {
@@ -20,11 +21,52 @@ class Instrument {
   }
 
   toObject() {
-    let object = {
+    return {
       name: this.name,
       tunings: this.tunings.map((tuning) => tuning.toObject())
     }
-    return object
+  }
+
+  display = (parent, layout, tuner) => {
+    for (const child of parent.childNodes) {
+      child.remove()
+    }
+    for (const func of layout.childElementFunctions) {
+      if (func()) {
+        func().remove()
+      }
+    }
+    const h1 = layout.instrumentNameH1()
+      ? layout.instrumentNameH1()
+      : document.createElement('h1')
+    h1.id = 'instrument-name'
+    h1.textContent = this.name
+    if (!layout.instrumentNameH1()) {
+      parent.append(h1)
+    }
+    Forms.showTuningForm(this, tuner, parent, layout)
+    return this
+  }
+
+  static createInstrumentFromJSON = (instrumentJSON, tuningsJSON) => {
+    const name = instrumentJSON.attributes.name
+    const tunings = tuningsJSON.data.map(
+      (tuning) => new Tuning(tuning.attributes.name, tuning.attributes.notes)
+    )
+    return new Instrument(name, tunings)
+  }
+
+  static async createInstrumentsFromJSON(instrumentsJSON, client) {
+    const instruments = []
+    for (const instrumentJSON of instrumentsJSON.data) {
+      const tuningsJSON = await client.getTunings(
+        `?filter[instrument_id_eq]=${instrumentJSON.id}`
+      )
+      instruments.push(
+        Instrument.createInstrumentFromJSON(instrumentJSON, tuningsJSON)
+      )
+    }
+    return instruments
   }
 }
 
