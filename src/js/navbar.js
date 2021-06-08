2c656b470a0f795d2bacbdacc5ab37565c6cf61c
ghemsley <ghemsley@protonmail.ch>
2021-06-08 08:00:08 -0500

    Refactor navbar creation logic into navbar.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,4 +1,5 @@
-const content = () => document.getElementById('content')
+import Forms from './forms'
+import Instrument from './instrument'
 
 class Navbar {
   constructor(title, sections = []) {
@@ -10,7 +11,7 @@ class Navbar {
     this.sections.push(sectionObject)
   }
 
-  appendToParent(parent) {
+  appendToParent(parent, layout) {
     const navbarDiv = document.createElement('div')
     const navbarUl = document.createElement('ul')
     const titleH1 = document.createElement('h1')
@@ -27,16 +28,22 @@ class Navbar {
     titleH1.classList.add('pure-link', 'pure-menu-heading')
     navbarDiv.appendChild(titleH1)
     const sectionsArray = this.sections.map((sectionObject) => {
+      const idString = sectionObject.name.toLowerCase().split(' ').join('-')
+
       const sectionLi = document.createElement('li')
       const sectionA = document.createElement('a')
       const sectionP = document.createElement('p')
-      sectionLi.id = `navbar-${sectionObject.name}-item`
-      sectionA.id = `navbar-${sectionObject.name}-link`
-      sectionP.id = `navbar-${sectionObject.name}-text`
+
+      sectionLi.id = `navbar-${idString}-item`
+      sectionA.id = `navbar-${idString}-link`
+      sectionP.id = `navbar-${idString}-text`
+
       sectionLi.classList.add('pure-menu-item')
       sectionA.classList.add('pure-menu-link')
       sectionP.textContent = sectionObject.name
+
       sectionA.appendChild(sectionP)
+
       if (sectionObject.link) {
         sectionA.href = sectionObject.link
       } else if (sectionObject.onClick) {
@@ -51,11 +58,55 @@ class Navbar {
     for (const section of sectionsArray) {
       navbarDiv.appendChild(section)
     }
-    if (content()) {
-      parent.insertBefore(navbarDiv, content().parentNode)
+    if (layout.content()) {
+      parent.insertBefore(navbarDiv, layout.content().parentNode)
     } else {
       parent.appendChild(navbarDiv)
     }
+  }
+
+  static createFromData = (parent, layout, client, tuner) => {
+    return client.getInstruments().then((instrumentJSON) => {
+      Instrument.createInstrumentsFromJSON(instrumentJSON, client).then(
+        (instruments) => {
+          const sections = []
+          for (const instrument of instruments) {
+            sections.push({
+              name: instrument.name,
+              onClick: instrument.display.bind(
+                instrument,
+                layout.content(),
+                layout,
+                tuner
+              )
+            })
+          }
+          sections.push(
+            {
+              name: 'New instrument',
+              onClick: Forms.createNewInstrumentForm.bind(
+                Forms,
+                layout.content(),
+                layout,
+                client
+              )
+            },
+            {
+              name: 'Add tuning to instrument',
+              onClick: Forms.createNewTuningForm.bind(
+                Forms,
+                layout.content(),
+                layout, 
+                client
+              )
+            }
+          )
+          const navbar = new Navbar('Instrument tuner', sections)
+          navbar.appendToParent(parent, layout)
+          return navbar
+        }
+      )
+    })
   }
 }
 
