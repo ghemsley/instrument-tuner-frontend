79cf0a6be2d759600f67404e28f827a78926880d
ghemsley <ghemsley@protonmail.ch>
2021-06-08 08:00:51 -0500

    Refactor tuning display logic into tuning.js

--- src/js/tuning.js
+++ src/js/tuning.js
@@ -5,8 +5,27 @@ class Tuning {
   }
 
   toObject() {
-    let object = { name: this.name, notes: this.notes }
-    return object
+    return { name: this.name, notes: this.notes }
+  }
+
+  display(layout) {
+    const h3 = layout.tuningNameH3()
+      ? layout.tuningNameH3()
+      : document.createElement('h3')
+    const h4 = layout.tuningNotesH4()
+      ? layout.tuningNotesH4()
+      : document.createElement('h4')
+    h3.id = 'tuning-name'
+    h4.id = 'tuning-notes'
+    h3.textContent = this.name
+    h4.textContent = this.notes.join(' ')
+    if (!layout.tuningNameH3()) {
+      layout.content().appendChild(h3)
+    }
+    if (!layout.tuningNotesH4()) {
+      layout.content().appendChild(h4)
+    }
+    return this
   }
 }
 
